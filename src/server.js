8be37845d5c6e244e4dda04ca490d52791f3a847
Michael Prather <mprather1@gmail.com>
2017-04-06 00:59:10 +0000

    0.1.3 -> get package name of client

--- src/server.js
+++ src/server.js
@@ -3,37 +3,53 @@ import logger from 'winston-color'
 import DuplexEmitter from 'duplex-emitter'
 import chalk from 'chalk'
 import json_data from './data.json'
+import pkg from '../package.json'
 
-const server = net.createServer()
-const port = process.env.PORT || 55445
-const hostname = process.env.HOSTNAME
+const options = {
+  server: net.createServer(),
+  port: process.env.PORT || 55445,
+  hostname: process.env.HOSTNAME,
+  packageName: pkg.name
+}
+
+const {server, packageName, hostname, port} = options
 
 server.on('listening', () => {
-  logger.info(chalk.magenta(`listening on port ${port}...`))
+  logger.info(`${chalk.bgBlack.cyan(packageName)} listening on port ${port}...`)
 })
 
-server.on('connection', handleConnection)
+server.on('connection', (data) => {
+  handleConnection(data)
+})
 
 server.listen(port, hostname)
 
 function handleConnection (socket) {
-  logger.info('client connected...')
+  const emitter = DuplexEmitter(socket)
   
   socket.on('error', handleError)
-  socket.on('close', handleClose)
   
-  const emitter = DuplexEmitter(socket)
+  emitter.once('connected', (data) => {
+    emitter.emit('connected', packageName)
+    logger.info(chalk.bgBlack.green(data), 'connected...')
+    
+    socket.on('close', (event) => {
+      handleClose(event, data)
+    })
+  })
+  
   emitter.emit('json-data', json_data)
+  
   emitter.on('complete', (data) => {
     emitter.emit('confirmaiton', 'confirmed received...')
-  logger.info(chalk.cyan(data))
+    logger.info(chalk.magenta(data))
   })
   
   function handleError (err) {
     logger.info("error:", err.message)
   }
   
-  function handleClose () {
-    logger.warn('connection closed...')
+  function handleClose (event, name) {
+    logger.warn(`${chalk.bgBlack.green(name)} disconnected...`)
   }
 }
